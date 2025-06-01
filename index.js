9deee9eaa30de2b30e71f20ea00c248f2c951fc4
NTKGaming <102576372+NTKGaming@users.noreply.github.com>
2025-06-01 20:53:30 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function hideIntro() {
+    
+    document.getElementById('Intro').style.display='none';
+    document.getElementById('body').style.background='none';
+    document.getElementById('loading').style.display='block';
+
+
+}
+
